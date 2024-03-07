1873dbb9c01cf242d494d5ef266f76963a4ba570
thonne <ttwange2@gmail.com>
2024-03-07 10:17:03 +0300

    building an orchestration of the tasks using prefect

--- flows/api_extract.py
+++ flows/api_extract.py
@@ -3,28 +3,24 @@ import psycopg2
 import requests
 import pandas as pd
 from dotenv import load_dotenv
+from prefect import flow,task
 
 load_dotenv()
 
-# PostgreSQL connection parameters from environment variables
-postgres_user = os.getenv("POSTGRES_USER")
-postgres_password = os.getenv("POSTGRES_PASSWORD")
-postgres_db = os.getenv("POSTGRES_DB")
-postgres_host = os.getenv("POSTGRES_HOST")
-postgres_port = os.getenv("postgres_PORT")
-
-
+@task(log_prints=True,retires=3)
 def fetch_data():
     url = "https://api.energidataservice.dk/dataset/PowerSystemRightNow/"
     data = requests.get(url).json()
     return data['records']
 
+@task(log_prints=True,retires=3)
 def transformation(json_data):
     df = pd.DataFrame(json_data)
     df = df.drop(columns=["aFRR_ActivatedDK1","aFRR_ActivatedDK2","mFRR_ActivatedDK1","mFRR_ActivatedDK2","ImbalanceDK1","ImbalanceDK2"])
     return df
 
-def load(clean_data):
+@task(log_prints=True,retires=3)
+def load(clean_data,postgres_user,postgres_password,postgres_host,postgres_port,postgres_db):
     postgres_connection_string = f"postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_db}"
 
     conn = psycopg2.connect(postgres_connection_string)
@@ -40,8 +36,19 @@ def load(clean_data):
     conn.commit()
     print("Data loaded successfully.")
 
-json_data = fetch_data()
-clean_data = transformation(json_data)
-load(clean_data)
+
+@flow(name="Energy ingest")
+def main_flow():
+    postgres_user = os.getenv("POSTGRES_USER")
+    postgres_password = os.getenv("POSTGRES_PASSWORD")
+    postgres_db = os.getenv("POSTGRES_DB")
+    postgres_host = os.getenv("POSTGRES_HOST")
+    postgres_port = os.getenv("postgres_PORT")
+
+    json_data = fetch_data()
+    clean_data = transformation(json_data)
+    load(clean_data,postgres_user,postgres_password,postgres_host,postgres_port,postgres_db)
+
+
 
 
