cf2776ec156d24b06e9be48193cddbcfa8bb5d71
thonne <ttwange2@gmail.com>
2024-03-16 10:20:28 +0300

    Refactor code to use Prefect flow instead of individual tasks.

--- flows/Kafka_Snowflake_workflow.py
+++ flows/Kafka_Snowflake_workflow.py
@@ -7,9 +7,10 @@ from prefect import flow, task
 
 load_dotenv()
 
-@task
-def connect_to_snowflake():
+@flow(name="Kafka_Snowflake")
+def snow_main():
     try:
+        # Connect to Snowflake
         conn = snowflake.connector.connect(
             user=os.getenv("snowflake_user"),
             password=os.getenv("snowflake_password"),
@@ -18,27 +19,17 @@ def connect_to_snowflake():
             database=os.getenv("snowflake_database"),
             schema=os.getenv("snowflake_schema")
         )
-        return conn
-    except Exception as e:
-        print(f"Error connecting to Snowflake: {e}")
+        cursor = conn.cursor()
 
-@task
-def consume_messages():
-    try:
+        # Kafka Consumer Configuration
         consumer = KafkaConsumer(
             os.getenv("KAFKA_TOPIC"),
             bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS"),
             group_id=os.getenv("KAFKA_GROUP_ID"),
             value_deserializer=lambda x: json.loads(x.decode('utf-8'))
         )
-        for message in consumer:
-            yield message.value
-    except Exception as e:
-        print(f"Error consuming messages from Kafka: {e}")
 
-@task
-def insert_data_to_snowflake(con, data):
-    try:
+        # Insert data into Snowflake
         snowflake_insert_sql = """
             INSERT INTO emission (
             Minutes1UTC, Minutes1DK, CO2Emission, ProductionGe100MW,
@@ -50,20 +41,18 @@ def insert_data_to_snowflake(con, data):
         ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
 
-        # Create a cursor from the Snowflake connection
-        cursor = con.cursor()
-        cursor.executemany(snowflake_insert_sql, data)
-        cursor.close()
-        con.commit()
+        for message in consumer:
+            try:
+                cursor.execute(snowflake_insert_sql, message.value)
+                conn.commit()
+                print("Data inserted into Snowflake")
+            except Exception as e:
+                print(f"Error inserting data to Snowflake: {e}")
+                conn.rollback()
     except Exception as e:
-        print(f"Error inserting data to Snowflake: {e}")
+        print(f"Error connecting to Snowflake: {e}")
 
-# Define the Prefect flow
-@flow(name="Kafka_snowflake")
-def snow_main():
-    snowflake_conn = connect_to_snowflake()
-    kafka_messages = consume_messages()
-    insert_data_to_snowflake(snowflake_conn, kafka_messages)
+
 
 if __name__ == "__main__":
     snow_main()
