0e448419c0a0aec17cccaa743dbdfb1827cf00b8
thonne <ttwange2@gmail.com>
2024-03-20 05:08:53 +0300

    Working on pulling data from kafka to dbt directly.

---
+++ dbt_transformation/models/staging/kafka_data.sql
@@ -0,0 +1,26 @@
+-- kafka_data_model.sql
+
+{{ config(
+  materialized='table',
+  alias='kafka_data'
+) }}
+
+select
+    Minutes1UTC,
+    CO2Emission,
+    ProductionGe100MW,
+    ProductionLt100MW,
+    SolarPower,
+    OffshoreWindPower,
+    OnshoreWindPower,
+    Exchange_Sum,
+    Exchange_DK1_DE,
+    Exchange_DK1_NL,
+    Exchange_DK1_GB,
+    Exchange_DK1_NO,
+    Exchange_DK1_SE,
+    Exchange_DK1_DK2,
+    Exchange_DK2_DE,
+    Exchange_DK2_SE,
+    Exchange_Bornholm_SE
+from {{ source('kafka', 'postgres.public.emission') }};
